028182e278578361a9d293f63e3eee3f7d5a0ef6
Michael-MD <mdei0001@student.monash.edu>
2022-02-17 23:20:47 +1100

    implement absorbing boundary conditions

--- 1D_TM.py
+++ 1D_TM.py
@@ -22,8 +22,14 @@ class one_dimensional_simulation:
 		self.mE=self.delta_t/(eps_0*self.delta_x)
 		self.mH=self.delta_t/(mu_0*self.delta_x)
 
+		self.eta=np.sqrt(mu_0/eps_0)
+
 		self.E=E_0
 		self.H=H_0
+		self.H_tilde=self.H*self.eta
+
+		
+
 
 		self.E_source=lambda n: 0
 
@@ -41,16 +47,52 @@ class one_dimensional_simulation:
 			num_time_steps=int((T-self.sim_time)/self.delta_t)
 
 		
+		self.E_edge_left=[0,0,0]
+		self.H_edge_left=[0,0,0]
+
+		self.E_edge_right=[0,0,0]
+		self.H_edge_right=[0,0,0]
+
+
 		for n in range(num_time_steps):
 			n+=int(self.sim_time/self.delta_t)
+
+
+			self.H[0]=self.H_edge_left[1]
+			self.H[self.N_x-1]=self.H_edge_right[2]
 			for k in range(0,self.N_x-1):
-				self.H[k]=self.H[k]+self.mH*( self.E[k+1] - self.E[k] )
-			self.H[self.N_x-1]=self.H[k]+self.mH*( 0 - self.E[k] )
+				self.H[k]=self.H[k]+self.mH*( self.E[k+1] - self.E[k] )			
 
 
-			self.E[0]=self.E[0]+self.mE*( self.H[0] - 0 )
+			self.H_tilde=self.H*self.eta
+
+
+			self.E[0]=self.E_edge_left[1]
+			self.E[self.N_x-1]=self.E_edge_right[2]
 			for k in range(1,self.N_x):
 				self.E[k]=self.E[k]+self.mE*( self.H[k] - self.H[k-1] )
+			
+
+			# ---------------------------------------------
+
+			self.H_edge_left[2]=self.H_edge_left[1]
+			self.H_edge_left[1]=self.H_edge_left[1]
+			self.H_edge_left[0]=self.H[1]
+
+			self.H_edge_right[2]=self.H_edge_right[1]
+			self.H_edge_right[1]=self.H_edge_right[1]
+			self.H_edge_right[0]=self.H[-2]
+
+			# ---------------------------------------------
+
+			self.E_edge_left[2]=self.E_edge_left[1]
+			self.E_edge_left[1]=self.E_edge_left[1]
+			self.E_edge_left[0]=self.E[1]
+
+			self.E_edge_right[2]=self.E_edge_right[1]
+			self.E_edge_right[1]=self.E_edge_right[1]
+			self.E_edge_right[0]=self.E[-2]
+
 
 			self.E[self.pos_center]=self.E_source(n)
 
@@ -61,18 +103,17 @@ class one_dimensional_simulation:
 
 
 x=one_dimensional_simulation(0.18e-9, 5.4e-2, np.zeros((401,1)), np.zeros((401,1)))
-x.E_source=lambda n: np.sin(2*np.pi*278e6*x.delta_t*n)
-# x.E_source=lambda n: np.exp(-((n-8)**2)/16)
+# x.E_source=lambda n: np.sin(2*np.pi*278e6*x.delta_t*n)
+x.E_source=lambda n: np.exp(-((n-8)**2)/16)
 
 
-for i in range(0,50):
-	x.step(i*10e-9)
+for i in range(0,50,20):
+	x.step(i*1e-9)
 
-	# plt.plot(x.E)
-	# plt.plot(x.H)
+	plt.plot(x.E)
+	# plt.plot(x.H_tilde)
 
-	
-	# plt.show()
+	plt.show()
 
 
 
